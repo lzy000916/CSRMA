c02bbe314ef9adad5d1bdc8593578b13f4a281cb
lzy000916 <m325123232@sues.edu.cn>
2025-02-21 13:41:11 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,19 +6,11 @@ if [ -f "$dir/Train.py" ];then
     pwd
     export CUDA_VISIBLE_DEVICES=0
     ##### run with Polyp Datasets
-    #python Train.py --model_name CSCAUNet --epoch 120 --batchsize 8 --trainsize 352 --train_save CSCAUNet_Kvasir_1e4_bs8_e120_s352_25 --lr 0.0001 --train_path $dir/data/TrainDataset --test_path $dir/data/TestDataset/Kvasir/
+    #python Train.py --model_name CSRMAUNet --epoch 120 --batchsize 8 --trainsize 352 --train_save CSRMAUNet_Kvasir_1e4_bs8_e120_s352_25 --lr 0.0001 --train_path $dir/data/TrainDataset --test_path $dir/data/TestDataset/Kvasir/
     #sleep 1m
-    python Test.py --train_save CSCAUNet_Kvasir_1e4_bs8_e120_s352_25 --testsize 352 --test_path $dir/data/TestDataset
+    python Test.py --train_save CSRMAUNet_Kvasir_1e4_bs8_e120_s352_25 --testsize 352 --test_path $dir/data/TestDataset
     
-    ### run with ISIC 2018
-    #python Train.py --model_name CSCAUNet --epoch 121 --batchsize 16 --trainsize 352 --train_save CSCAUNet_ISIC_1e4_bs16_e120_s352 --lr 0.0001 --train_path $dir/data/ISIC2018/train --test_path $dir/data/ISIC2018/val/
-   # sleep 1m
-   # python Test.py --train_save CSCAUNet_ISIC_1e4_bs16_e120_s352 --testsize 352 --test_path $dir/data/ISIC2018
-
-    ### run with 2018 DSB
-    #python Train.py --model_name CSCAUNet --epoch 121 --batchsize 16 --trainsize 256 --train_save CSCAUNet_DSB_1e4_bs16_e120_s256 --lr 0.0001 --train_path $dir/data/2018DSB/train --test_path $dir/data/2018DSB/val/
-    #sleep 1m
-    #python Test.py --train_save CSCAUNet_ISIC_1e4_bs16_e120_s256 --testsize 256 --test_path $dir/data/2018DSB
+  
 else
     echo "file not exists"
 fi
